172a3a72db2c989ea35fc56842f46d7714dfe0b0
Craig Campbell <iamcraigcampbell@gmail.com>
2018-05-13 21:58:26 -0400

    Grab the version from package.json

--- src/bomb.js
+++ src/bomb.js
@@ -1,13 +1,14 @@
 import { runTests } from './runner';
 const fs = require('fs');
 const argv = require('yargs').argv;
+const version = require('./../package.json').version;
 
 function showUsage() {
     console.log([
         '        ,--.!,',
         '     __/   -*-',
         '   ,d08b.  \'|`',
-        '   0088MM          BOMB v1.0.0',
+        `   0088MM          BOMB v${version}`,
         '   `9MMP\''].join('\n'));
     console.log('\n\x1B[1mUSAGE\x1B[0m');
     console.log('bomb /path/to/tests');
