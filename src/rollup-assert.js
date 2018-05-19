05f1a3b27d2a3af1ba00d7408c30d6ae93114ff9
Craig Campbell <iamcraigcampbell@gmail.com>
2018-05-18 21:33:55 -0400

    Move a couple functions to util

--- src/rollup-assert.js
+++ src/rollup-assert.js
@@ -1,27 +1,7 @@
+import { escapeQuotes, findLineAndColumnForPosition } from './util';
 import MagicString from 'magic-string';
 const esprima = require('esprima');
 const escodegen = require('escodegen');
-
-function findLineAndColumnForPosition(code, index) {
-    const lines = code.split('\n');
-    let pos = 0;
-    let lastPos = 0;
-    let line = 0;
-    let column = 0;
-    while (pos < index) {
-        const nextLine = lines.shift();
-        line += 1;
-        lastPos = pos;
-        pos += nextLine.length + 1; // 1 for the \n
-    }
-
-    column += (index - lastPos) + 1; // 1 to not be zero indexed
-    return { line, column }
-}
-
-function escapeQuotes(string) {
-    return string.replace(/'/g, "\\'");
-}
 
 function getReplacement(assertCode, file, position, index) {
     const ast = esprima.parse(assertCode);
